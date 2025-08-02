cd08b0c6705ba358a0ca9ac2150b9e22a4064ca4
vicLin8712 <ac01969454@gmail.com>
2025-08-02 20:35:50 +0800

    Add pre-commit hook

--- lib/mm/mm.c
+++ lib/mm/mm.c
@@ -1,27 +1,30 @@
 #include "mm.h"
 
-void *memset( void *buf, char c, size_t n){
-	uint8_t *p = (uint8_t *) buf;
-	while( n--)
-		*p++ = c;
-	return buf;
+void *memset(void *buf, char c, size_t n)
+{
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
 }
 
-char *memcpy(char *dst, const char *src, size_t n){
-	uint8_t *d = (uint8_t *)dst;
-	const uint8_t *s = (const uint8_t *)src;
-	while(n--){
-		*d++ = *s++;
-	}
-	return dst;
+char *memcpy(char *dst, const char *src, size_t n)
+{
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dst;
 }
 
-int memcmp(const char *s1, const char *s2){
-	while (*s1 && *s2){
-		if (*s1 != *s2)
-			break;
-		s1 ++;
-		s2 ++;
-	}
-	return *(unsigned char *)s1 - *(unsigned char *)s2;
+int memcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *) s1 - *(unsigned char *) s2;
 }
