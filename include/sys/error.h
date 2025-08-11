6d05be38f6f695fb460c16578c722f9c03b02ec1
vicLin8712 <ac01969454@gmail.com>
2025-08-11 14:53:37 +0800

    Add error table

---
+++ include/sys/error.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "type.h"
+
+/* This file include all error codes */
+enum {
+    /* Success and general fail code */
+    ERR_OK = 0,     /* Operation successful */
+    ERR_FAIL = 1,   /* General fail */
+
+    /* Task Management and Scheduler Error (auto-numbered from -16383) */
+    ERR_NO_TASK = -16383,  /* No task for scheduling */
+    ERR_KCB_ALLOC,         /* Kernel control block allocation fail */
+    ERR_TCB_ALLOC,         /* Thread control block allocation fail */
+    ERR_STACK_ALLOC,       /* Task stack allocation fail */
+
+    /* Memory Protection Error*/
+    ERR_STACK_CHECK       /* Stack overflow or corruption detected */
+
+};
+
+struct err_code {
+    int32_t code;         /* Error code */
+    char *const desc;     /* Decode table pointer, never should be modified */
+};
+
+extern const struct err_code *const perror;
---
+++ kernel/error.c
@@ -0,0 +1,17 @@
+#include "sys/error.h"
+
+static const struct err_code desc[] = {
+    /* Success and general fail code */
+    {ERR_OK ,"no error"},     /* Operation successful */
+    {ERR_FAIL, "generic failure" },   /* General fail */
+
+    /* Task Management and Scheduler Error (auto-numbered from -16383) */
+    {ERR_NO_TASK, "no task for scheduling" },  /* No task for scheduling */
+    {ERR_KCB_ALLOC,"KCB malloc fail"},         /* Kernel control block allocation fail */
+    {ERR_TCB_ALLOC,"TCB malloc fail"},         /* Thread control block allocation fail */
+    {ERR_STACK_ALLOC,"Task stack malloc fail"},       /* Task stack allocation fail */
+
+    /* Memory Protection Error*/
+    {ERR_STACK_CHECK, "Stack malloc overflow or corruption"},       /* Stack overflow or corruption detected */
+
+};
