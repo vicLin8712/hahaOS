91d9cc469499188eab2fa3203dddb012db64b932
vicLin8712 <ac01969454@gmail.com>
2025-09-12 16:06:54 +0800

    Revise task control structure
    In this commit, task priority concept is introduced for the purpose of
    O(1) scheduler.
    The tcb also modified for the data collecting, timer for R and blocked
    reason for the future coherencing issues.
    sched block is used for management all tasks with different state,
    providing bitmap for the purpose of task searching in O(1) complexity.
    Finally, kcb revised for MSP future design.

--- include/sys/task.h
+++ include/sys/task.h
@@ -1,43 +1,78 @@
 #pragma once
 #include "hal.h"
+#include "list.h"
 #include "type.h"
 
+/* Stack size and begining */
 extern uint32_t __stack_top;
+#define STACK_SIZE 8192
 
-/* Max number of tcb */
-#define PROCS_MAX 8
+/* Define priority level of task */
+#define PRIOR_LEVEL 8
 
-/* End tcb (can put new tcb)*/
-#define NO_TASK 0
-/* Ready to run tcb*/
-#define TASK_READY 1
-/* Runnable tcb */
-#define TASK_RUNNING 2
-/* Stop tcb (can be resumed)*/
-#define TASK_STOPPED 3
-/* Postpone task */
-#define TASK_POSTPONE 4
+/* Task state definition */
+#define TASK_RUNNING 0   /* The task executing on the CPU */
+#define TASK_READY 1     /* The tasks ready for scheduler selecting */
+#define TASK_BLOCKED 2   /* The task blocked and wait for event happen */
+#define TASK_SUSPENDED 3 /* The tasks stop itself */
+#define TASK_DELETED                                         \
+    4 /* Deleted tasks, wait for idle task to recycle memory \
+       */
 
+/* Task control block */
 typedef struct tcb {
-    uint32_t entry; /* new function entry address */
-    int32_t pid;
-    int32_t state;
+    /* Task basic properties */
+    int32_t id;
+    uint8_t state;
+    uint8_t prio;
+    list_node_t cur_node; /* Task node at queue*/
+    uint32_t stack[STACK_SIZE];
+
+    /* Time relative */
+    uint64_t total_exe_ticks;
+    uint64_t time_slice_left; /* RR remaining ticks */
+    uint64_t wakeup_tick;     /* Absolute tick to wakeup */
+
+    /* Context switch relative */
     jmp_buf context;
-    uint32_t stack[8192];
-    uint32_t task_index;
+    uint64_t ctxsw_irq;   /* Interrupt times*/
+    uint64_t ctxsw_total; /* Total context switch times */
+    uint8_t block_reason; /* Interrupt reasons */
+
 } tcb_t;
 
+typedef struct sched {
+    /* All tasks lists */
+    list_t *task_list;          /* List for all tasks, including DELETED */
+    list_t *ready[PRIOR_LEVEL]; /* Ready for picked up tasks */
+    list_t *wait;               /* Wait event tasks */
+    list_t *suspend;            /* Suspended by itself tasks */
+
+    /* Tasks list properties */
+    uint8_t bitmap;                    /* Bit map for O(1) scheduler */
+    uint32_t tasks_count[PRIOR_LEVEL]; /* Number of tasks in each queue */
+    uint64_t ctxsw_total;              /* Total context switch times */
+    uint64_t total_exe_ticks;
+
+    /* Special tasks */
+    tcb_t *idle; /* Idle task */
+
+} sched_t;
+
 typedef struct {
-    struct tcb *tcbs;    /* Data structure store all tcbs */
-    struct tcb *cur_tcb; /* Current running tcb */
-    uint8_t pid_assign;  /* Check pid and assign to new tcb */
-    jmp_buf context;
+    int hart_id;             /* CPU hart ID */
+    struct tcb *cur_tcb;     /* Task control block of current task */
+    struct sched *sched;     /* Scheduler control block*/
+    uint8_t pid_assign;      /* Check pid and assign to new tcb */
+    jmp_buf context;         /* Kernel state context */
+    bool preemptive;         /* Preemptive option */
+    volatile uint32_t ticks; /* Global system tick, incremented by timer */
 
 } kcb_t;
 
+
 extern kcb_t *kcb;
 
-extern struct tcb tcbs[PROCS_MAX];
 /* Scheduler */
 int32_t create_task(uint32_t pc);
 /* Find next available tcb*/
